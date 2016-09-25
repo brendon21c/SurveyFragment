bfbd0adb84457802f3e7e5b11843a372798c4814
Brendon <brennon.mckeever@gmail.com>
2016-09-25 18:24:15 -0500

    Initial commit

---
+++ app/src/main/java/com/brendon/surveyfragment/SurveyFragment.java
@@ -0,0 +1,70 @@
+package com.brendon.surveyfragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.HashMap;
+
+/**
+ * Created by Brendon on 9/25/16.
+ */
+
+public class SurveyFragment extends Fragment {
+
+    private TextView mQuestion;
+    private Button mButton1;
+    private Button mButton2;
+
+    private HashMap<String, Integer> mSurveybank = new HashMap<String, Integer>();
+    private String mQuestionText;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+        View view = inflater.inflate(R.layout.survey_fragment, container, false);
+
+        mQuestion = (TextView) view.findViewById(R.id.survey_question);
+        mButton1 = (Button) view.findViewById(R.id.yes_button);
+        mButton2 = (Button) view.findViewById(R.id.no_button);
+
+        Bundle bundle = this.getArguments();
+
+        if (bundle.getSerializable("hash key") != null) {
+
+            mSurveybank = (HashMap<String,Integer>) bundle.getSerializable("hash key");
+
+        }
+
+        mQuestionText = bundle.getString("Question key");
+        mQuestion.setText(mQuestionText);
+        mButton1.setText(SurveyMain.mAnswerkey1);
+        mButton2.setText(SurveyMain.mAnswerkey2);
+
+
+        mButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int temp = mSurveybank.get(SurveyMain.mAnswerkey1);
+
+                mSurveybank.put(SurveyMain.mAnswerkey1, temp + 1);
+
+            }
+        });
+
+
+
+
+
+        return view;
+
+    }
+}
---
+++ app/src/main/java/com/brendon/surveyfragment/SurveyMain.java
@@ -0,0 +1,102 @@
+package com.brendon.surveyfragment;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.HashMap;
+
+public class SurveyMain extends FragmentActivity {
+
+    private static final String TAG = "MainActivity";
+    public static final String INDEX_KEY= "Survey bank";
+    private static final String QUESTION_INDEX_KEY = "Question key";
+    private static final String HASH_INDEX_KEY = "Hash key";
+    private static final int SURVEY_REQUEST_CODE = 1;
+    private static final int UPDATE_REQUEST_CODE = 2;
+
+
+    /*
+   This section is to provide a starting point for the values
+    */
+    private static final int  mAnswerStart1 = 0;
+    private static final int mAnswerStart2 = 0;
+    private String answerTemp = "";
+
+    public static String mCurrentSurveyQuestion = "Do you like fresh baked chocolate chip cookies?";
+    public static String mAnswerkey1 = "yes";
+    public static String mAnswerkey2 = "no";
+
+    /*
+    Survey answer total will be kep in this Hashmap.
+     */
+    private HashMap<String,Integer> surveyBank;
+
+    private Button mQuestionButton;
+    private Button mUpdateButton;
+    private Button mResultsButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey_main);
+
+        mQuestionButton = (Button) findViewById(R.id.survey_button);
+        mUpdateButton = (Button) findViewById(R.id.update_button);
+        mResultsButton = (Button) findViewById(R.id.results_button);
+
+
+
+        mQuestionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                FragmentManager fm = getSupportFragmentManager();
+                Fragment frag = fm.findFragmentById(R.id.activity_survey_main);
+
+
+                Bundle bundle = new Bundle();
+                bundle.putSerializable(HASH_INDEX_KEY, surveyBank);
+                bundle.putString(QUESTION_INDEX_KEY, mCurrentSurveyQuestion);
+
+                if (frag == null) {
+
+                    frag = new SurveyFragment();
+                    frag.setArguments(bundle);
+
+                    fm.beginTransaction()
+                            .add(R.id.activity_survey_main, frag)
+                            .addToBackStack(null)
+                            .commit();
+                }
+
+            }
+        });
+
+        createSurvey();
+
+
+
+    }
+
+    // Updates the surveyBank
+    private void createSurvey() {
+
+        if (surveyBank == null) {
+
+            surveyBank = new HashMap<String, Integer>();
+            surveyBank.put(mAnswerkey1, mAnswerStart1);
+            surveyBank.put(mAnswerkey2, mAnswerStart2);
+
+        }
+
+    }
+
+
+
+}
